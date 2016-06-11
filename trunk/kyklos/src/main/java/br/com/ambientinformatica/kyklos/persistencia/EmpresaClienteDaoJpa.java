2f1ab64975b08e6a9938817957c094d6666d8f33
gustavo.simoes <gustavo.simoes@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2016-06-11 23:40:32 +0000

    Ajuste no cadastro de uma nova empresa, vinculado a mesma pela Entidade PessoaEmpresa.

--- trunk/kyklos/src/main/java/br/com/ambientinformatica/kyklos/persistencia/EmpresaClienteDaoJpa.java
+++ trunk/kyklos/src/main/java/br/com/ambientinformatica/kyklos/persistencia/EmpresaClienteDaoJpa.java
@@ -1,31 +1,18 @@
 package br.com.ambientinformatica.kyklos.persistencia;
 
+import javax.persistence.NoResultException;
 import javax.persistence.Query;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 import br.com.ambientinformatica.corporativo.entidade.Pessoa;
 import br.com.ambientinformatica.jpa.persistencia.PersistenciaJpa;
 import br.com.ambientinformatica.kyklos.entidade.EmpresaCliente;
-import br.com.ambientinformatica.kyklos.entidade.EmpresaUsuario;
-import br.com.ambientinformatica.kyklos.entidade.PedidoException;
-import br.com.ambientinformatica.kyklos.entidade.Usuario;
 
 @Repository("empresaClienteDao")
 public class EmpresaClienteDaoJpa extends PersistenciaJpa<EmpresaCliente> implements EmpresaClienteDao {
 
    private static final long serialVersionUID = 1L;
-   
-   @Autowired
-   private PessoaDao pessoaDao;
-   
-   @Autowired
-   private UsuarioDao usuarioDao;
-   
-   @Autowired
-   private EmpresaUsuarioDao empresaUsuarioDao;
    
    public EmpresaCliente consultarPorPessoa(Pessoa pessoa){
       Query query = em.createQuery("select e from EmpresaCliente e where e.pessoa = :pessoa");
@@ -33,37 +20,50 @@ public class EmpresaClienteDaoJpa extends PersistenciaJpa<EmpresaCliente> implem
       return (EmpresaCliente) query.getSingleResult();
    }
 
-   @Override
-   @Transactional(rollbackFor=PedidoException.class)
-   public void incluirNovoUsuario(EmpresaCliente empresaCliente, String senha) throws PedidoException {
-      Pessoa pessoaConsultada = pessoaDao.consultarPorCpfCnpj(empresaCliente.getPessoa().getCpfCnpj());
-      Usuario usuarioConsultado = usuarioDao.consultarPorLogin(empresaCliente.getPessoa().getEmail());
-      if(usuarioConsultado == null){
-         if(pessoaConsultada != null){
-            EmpresaCliente ec = consultarPorPessoa(pessoaConsultada);
-            if(ec != null){
-               throw new PedidoException("Este cnpj já está cadastrado");
-            }
-            empresaCliente.setPessoa(pessoaConsultada);
-         }else{
-         }
-         Usuario usuario = new Usuario();
-         usuario.setLogin(empresaCliente.getPessoa().getEmail());
-         usuario.setSenhaNaoCriptografada(senha);
-         
-         EmpresaUsuario empUsu = new EmpresaUsuario();
-         empUsu.setEmpresa(empresaCliente);
-         empUsu.setPrincipal(true);
-         empUsu.setUsuario(usuario);
-         
-         usuarioDao.incluir(usuario);
-         incluir(empresaCliente);
-         empresaUsuarioDao.incluir(empUsu);
-         
-      }else{
-         throw new PedidoException(String.format("Usuário %s já cadastrado. Logue e registre uma nova empresa.", empresaCliente.getPessoa().getEmail()));
+   public EmpresaCliente consultarPorCpfCnpj(String cpfCnpj){
+      try{
+      Query query = em.createQuery("select e from EmpresaCliente e where e.pessoa.cpfCnpj = :cpfCnpj");
+      query.setParameter("cpfCnpj", cpfCnpj);
+      return (EmpresaCliente) query.getSingleResult();
+      }catch(NoResultException nre){
+         return null;
       }
    }
+   
+//   @Override
+//   @Transactional(rollbackFor=PedidoException.class)
+//   public void incluirNovoUsuario(EmpresaCliente empresaCliente, String senha) throws PedidoException {
+//      
+//      Pessoa pessoaConsultada = pessoaDao.consultarPorCpfCnpj(empresaCliente.getPessoa().getCpfCnpj());
+//      Usuario usuarioConsultado = usuarioDao.consultarPorLogin(empresaCliente.getPessoa().getEmail());
+//      
+//      if(usuarioConsultado == null){
+//         if(pessoaConsultada != null){
+//            EmpresaCliente ec = consultarPorPessoa(pessoaConsultada);
+//            if(ec != null){
+//               throw new PedidoException("Este cnpj já está cadastrado");
+//            }
+//            empresaCliente.setPessoa(pessoaConsultada);
+//         }else{
+//            empresaCliente = empresaClienteDao.alterar(empresaCliente);
+//         }
+//         Usuario usuario = new Usuario();
+//         usuario.setLogin(empresaCliente.getPessoa().getEmail());
+//         usuario.setSenhaNaoCriptografada(senha);
+//         usuario.setNome(empresaCliente.getPessoa().getNome());
+//         usuarioDao.incluir(usuario);
+//         
+//         EmpresaUsuario empresaUsuario = new EmpresaUsuario();
+//         empresaUsuario.setEmpresa(empresaCliente);
+//         empresaUsuario.setPrincipal(true);
+//         empresaUsuario.setUsuario(usuario);
+//         
+//         empresaUsuarioDao.incluir(empresaUsuario);
+//         
+//      }else{
+//         throw new PedidoException(String.format("Usuário %s já cadastrado. Logue e registre uma nova empresa.", empresaCliente.getPessoa().getEmail()));
+//      }
+//   }
 
 
 
