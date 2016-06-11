5288781acf76654ff47887b4f1036ca7fc8de7ed
gustavo.simoes <gustavo.simoes@5ca51ab9-5e42-4d65-87f8-9b45aafc4836>
2016-06-11 23:40:32 +0000

    Ajuste no cadastro de uma nova empresa, vinculado a mesma pela Entidade PessoaEmpresa.

--- src/main/java/br/com/ambientinformatica/kyklos/controle/CadastroUsuarioControl.java
+++ src/main/java/br/com/ambientinformatica/kyklos/controle/CadastroUsuarioControl.java
@@ -1,6 +1,7 @@
 package br.com.ambientinformatica.kyklos.controle;
 
 import java.io.Serializable;
+import java.util.Date;
 
 import javax.inject.Named;
 
@@ -10,7 +11,11 @@ import org.springframework.context.annotation.Scope;
 import br.com.ambientinformatica.ambientjsf.util.UtilFaces;
 import br.com.ambientinformatica.kyklos.entidade.EmpresaCliente;
 import br.com.ambientinformatica.kyklos.entidade.PedidoException;
+import br.com.ambientinformatica.kyklos.entidade.PessoaEmpresa;
+import br.com.ambientinformatica.kyklos.entidade.Usuario;
 import br.com.ambientinformatica.kyklos.persistencia.EmpresaClienteDao;
+import br.com.ambientinformatica.kyklos.persistencia.PessoaEmpresaDao;
+import br.com.ambientinformatica.kyklos.persistencia.UsuarioDao;
 
 @Named("CadastroUsuarioControl")
 @Scope("conversation")
@@ -21,6 +26,12 @@ public class CadastroUsuarioControl implements Serializable{
    @Autowired
    private EmpresaClienteDao empresaClienteDao;
 
+   @Autowired
+   private UsuarioDao usuarioDao;
+
+   @Autowired
+   private PessoaEmpresaDao pessoaEmpresaDao;
+   
    private EmpresaCliente empresaCliente = new EmpresaCliente();
 
    private String senha;
@@ -30,7 +41,33 @@ public class CadastroUsuarioControl implements Serializable{
    public void cadastrarPessoaEmpresaEUsuario(){
       try{
          if(senha != null && senha.equals(senhaConfirm)){
-            empresaClienteDao.incluirNovoUsuario(empresaCliente, senha);
+            Usuario usuario = new Usuario();
+            
+            EmpresaCliente empresaClienteConsultada = empresaClienteDao.consultarPorCpfCnpj(empresaCliente.getPessoa().getCpfCnpj());
+            if(empresaClienteConsultada == null){
+               empresaClienteConsultada = empresaClienteDao.alterar(empresaCliente);
+            }
+            
+            usuario = usuarioDao.consultarPorLogin(empresaCliente.getPessoa().getEmail());
+            if (usuario == null) {
+               usuario = new Usuario();
+               usuario.setAtivo(false);
+               usuario.setDataAlteracaoSenha(new Date());
+               usuario.setDataCriacao(new Date());
+               usuario.setLogin(empresaCliente.getPessoa().getEmail());
+               usuario.setNome(empresaCliente.getPessoa().getNome());
+               usuario.setSenhaNaoCriptografada(senha);
+               usuarioDao.incluir(usuario);
+            }else{
+               throw new PedidoException("Já existe um usuário cadastrado com esse email.");
+            }
+            
+            PessoaEmpresa pessoaEmpresa = new PessoaEmpresa();
+            pessoaEmpresa.setEmpresa(empresaClienteConsultada);
+            pessoaEmpresa.setPessoa(empresaClienteConsultada.getPessoa());
+            
+            pessoaEmpresaDao.incluir(pessoaEmpresa);
+            
             empresaCliente = new EmpresaCliente();
             UtilFaces.addMensagemFaces("Cadastro realizado com Sucesso! O link de ativação de cadastro foi enviado para o seu e-mail!");
          }else{
--- src/main/java/br/com/ambientinformatica/kyklos/persistencia/EmpresaClienteDao.java
+++ src/main/java/br/com/ambientinformatica/kyklos/persistencia/EmpresaClienteDao.java
@@ -1,11 +1,15 @@
 package br.com.ambientinformatica.kyklos.persistencia;
 
+import br.com.ambientinformatica.corporativo.entidade.Pessoa;
 import br.com.ambientinformatica.jpa.persistencia.Persistencia;
 import br.com.ambientinformatica.kyklos.entidade.EmpresaCliente;
-import br.com.ambientinformatica.kyklos.entidade.PedidoException;
 
 public interface EmpresaClienteDao extends Persistencia<EmpresaCliente>{
 
-   void incluirNovoUsuario(EmpresaCliente empresaCliente, String senha) throws PedidoException;
+//   void incluirNovoUsuario(EmpresaCliente empresaCliente, String senha) throws PedidoException;
+   
+   EmpresaCliente consultarPorPessoa(Pessoa pessoa);
+   
+   EmpresaCliente consultarPorCpfCnpj(String cpfCnpj);
    
 }
